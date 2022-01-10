120ccc1d47403187a4a50e812aebadbc57ce0630
lpjent <97470040+lpjent@users.noreply.github.com>
2022-01-10 14:23:29 -0500

    Add files via upload

---
+++ ExplosionScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+public class ExplosionScript : NetworkBehaviour {
+    List<GameObject> objectsAffected = new List<GameObject>();
+    public int damage;
+    public float explosionSize;
+    public float explosionForce;
+    public Vector3 shooterPosition;
+
+    void Start() {
+        //On esplosion creation, checks for closeness to the explosion
+        StartCoroutine(TurnOffCollider());
+        Destroy(gameObject, 3.0f);
+        GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+        foreach (GameObject player in players)
+        {
+            //Determines who has been booped by the explosion, and exerts force on them.
+            if (Vector3.Distance(player.transform.position, gameObject.transform.position) <= explosionSize)
+            {
+                player.GetComponent<PlayerManager>().RpcGetBoopedSon(explosionSize, explosionForce, gameObject.transform.position, shooterPosition);
+            }
+        }
+    }
+
+    IEnumerator TurnOffCollider(){
+        //Turns off the colider for the created explosion.
+        yield return new WaitForSeconds(0.1f);
+        gameObject.GetComponent<SphereCollider>().enabled = false;
+    }
+
+	void OnTriggerStay(Collider collision) {
+        //All damage to the terrain is on the server. If this isn't the server, stop this.
+        if (!isServer)  {
+            return;
+        }
+
+		//Damages terrain.
+		if (collision.gameObject.CompareTag("HexCell") && !objectsAffected.Contains(collision.gameObject)) {
+			terrainHealth health = collision.gameObject.GetComponent<terrainHealth>();
+			health.CmdTakeDamage(damage);
+
+			objectsAffected.Add(collision.gameObject);
+		}
+	}
+
+}
---
+++ bullet.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class bullet : NetworkBehaviour {
+
+	public int damage;
+    public float damageSize;
+	public float explosionSize;
+	public float explosionForce;
+	public GameObject explosionPrefab;
+    public string weaponName;
+    public Vector3 shooterPosition;
+
+
+
+	void OnCollisionEnter(Collision collision) {
+        //Checks the collision to see if it hit something and will explode.
+        if (collision.gameObject.tag == "Player" || collision.gameObject.tag == "HexCell" || collision.gameObject.tag == "bullet") {
+            CmdMakeExplosion();
+        }
+        // end if
+	}
+
+	[Command]
+	void CmdMakeExplosion() {
+        //Creating an explosion when the bullet hits. Uses explosion class object.
+        Vector3 localPosition = gameObject.transform.position;
+		GameObject explosion = Instantiate(explosionPrefab,localPosition,Quaternion.identity);
+        explosion.transform.localScale = new Vector3 (damageSize, damageSize, damageSize);
+		ExplosionScript explosionStats = explosion.GetComponent<ExplosionScript>();
+		explosionStats.damage = damage;
+		explosionStats.explosionSize = explosionSize;
+		explosionStats.explosionForce = explosionForce;
+        explosionStats.shooterPosition = shooterPosition;
+		NetworkServer.Spawn(explosion);
+        RpcPlayExplosionPFX(explosion, weaponName);
+    }
+
+    [ClientRpc]
+    void RpcPlayExplosionPFX(GameObject explosion, string name) {
+        //Client side plays explosion fx.
+        ParticleSystem[] pfxList = explosion.GetComponent<PFXList>().pfxList;
+        Debug.LogError("I am starting an explosion PFX. " + name);
+        switch (name)
+        {
+            case "Rocket Launcher":
+                pfxList[0].Play();
+                break;
+            case "Energy Rifle":
+                pfxList[1].Play();
+                break;
+            case "Sniper":
+                pfxList[2].Play();
+                break;
+            case "Lightning":
+                pfxList[3].Play();
+                break;
+            case "Plasma Ray":
+                break;
+            default:
+                Debug.LogError("Invalid Gun Name in Explosion PFX");
+                break;
+        }
+        explosion.GetComponent<ExplosionAudioManager>().Play(name);
+        //gets rid of the bullet.
+        Destroy(gameObject);
+    }
+}
